557400f1058aa8a1fd9340e23d9a3de8341f5cc8
mohsinexe <mohsinbehzad14@gmail.com>
2024-06-24 05:25:47 +0500

    mixin added

---
+++ mixin.dart
@@ -0,0 +1,19 @@
+// mixin is simply a class with no constructor
+// mixin is used for code reusability just like inheritance
+// with keyword is used instead of extends keyword as in inheritance
+
+class Player with Name,Age {
+  void position(){}
+}
+
+class Manager with Name, Age {
+  void Experience(){}
+} 
+
+mixin Name{
+  void name(){}
+}
+
+mixin Age{
+  void age(){}
+}
