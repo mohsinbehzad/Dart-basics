b68b3677167239ac89448471baf08f6a0c44b228
mohsinexe <mohsinbehzad14@gmail.com>
2024-06-30 14:13:40 +0500

    Polymorphism added

---
+++ polymorphism.dart
@@ -0,0 +1,32 @@
+// It is also known as overriding, means the parent and child class have same methods(same name and type).  @override is used as mentioned below.
+
+void main(){
+  Player p1;
+  p1 = new Footballer();
+  p1.display();
+
+  Player p2 = new Wrestler();
+  p2.display();
+}
+
+class Player{
+  @override
+  void display(){
+    print('Player');
+  }
+}
+
+class Footballer extends Player{
+  @override
+  void display(){
+    super.display();                                            // it will display parent class display
+    print('Footballer');
+  }
+}
+
+class Wrestler extends Player{
+  @override
+  void display(){
+    print('Wrestler');
+  }
+}
